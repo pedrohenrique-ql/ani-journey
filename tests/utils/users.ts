7c8aef5e06545987fcea65115101f487538103b9
Pedro Henrique <pedrohenriqueql.dev@gmail.com>
2024-09-10 21:47:00 -0300

    test: auth login

---
+++ tests/utils/users.ts
@@ -0,0 +1,22 @@
+import prisma from '@/database/prismaClient';
+import { CreateUserInput } from '@/modules/users/validators/CreateUserValidator';
+import { hashPassword, verifyPassword } from '@/utils/auth';
+import { createId } from '@paralleldrive/cuid2';
+
+export async function createUser(partialUser: Partial<CreateUserInput> = {}) {
+  const { password, ...restPartialUser } = partialUser;
+  const userPassword = password ?? 'password';
+  const hashedPassword = await hashPassword(userPassword);
+
+  const createdUser = await prisma.user.create({
+    data: {
+      id: createId(),
+      username: 'user',
+      email: 'email@test.com',
+      password: hashedPassword,
+      ...restPartialUser,
+    },
+  });
+
+  return { user: createdUser, password: userPassword };
+}
