9c7d0a70d4fb91307caa4c5c92aac6b6483bd3ac
Pedro Henrique <pedrohenriqueql.dev@gmail.com>
2024-09-15 23:30:55 -0300

    test: auth logout

--- src/modules/auth/router.ts
+++ src/modules/auth/router.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import AuthController from './AuthController';
+import ensureAuthenticated from '@/middlewares/ensureAuthenticated';
 
 const authRouter = Router();
 const authController = new AuthController();
 
 authRouter.post('/auth/login', authController.login);
-authRouter.post('/auth/refresh', authController.refresh);
-authRouter.post('/auth/logout', authController.logout);
+authRouter.post('/auth/refresh', ensureAuthenticated, authController.refresh);
+authRouter.post('/auth/logout', ensureAuthenticated, authController.logout);
 
 export default authRouter;
