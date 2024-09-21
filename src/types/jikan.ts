b650ade634784d09e69543038b1cbdf956f6d699
Pedro Henrique <pedrohenriqueql.dev@gmail.com>
2024-09-20 21:31:49 -0300

    test: anime search

---
+++ src/types/jikan.ts
@@ -0,0 +1,4 @@
+import { JikanComponents } from '@/types/jikanSchema';
+
+export type JikanAnimeSearchResponse = JikanComponents['schemas']['anime_search'];
+export type JikanAnimeGetByIdResponse = JikanComponents['schemas']['anime'];
